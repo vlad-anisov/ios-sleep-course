8e6218390aba69b39b30f917a6769a7619b50ccb
vlad <anisoffgo@gmail.com>
2025-10-21 01:51:07 +0300

    Initial Commit

---
+++ sleep course/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sleep course
+//
+//  Created by Vlad Anisov on 21.10.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sleep course/sleep_courseApp.swift
@@ -0,0 +1,17 @@
+//
+//  sleep_courseApp.swift
+//  sleep course
+//
+//  Created by Vlad Anisov on 21.10.25.
+//
+
+import SwiftUI
+
+@main
+struct sleep_courseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
