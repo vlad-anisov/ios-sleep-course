f5c2db09286ceee04891721d46284e64c86f45fb
vlad <anisoffgo@gmail.com>
2025-10-25 04:44:05 +0300

    25.10

--- sleep course/Extensions/Color+Theme.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-extension Color {
-    // Цвета из Assets.xcassets
-    static let appBackground = Color("BackgroundColor")
-    static let messageBubbleEva = Color("MessageBubbleColor")
-}
-
